2a98f9c9880556d017f2f0d178333a19d2c93af1
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2025-08-10 23:08:05 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,24 @@
+function myfunction(){
+    console.log("hey this side kritik");
+    console.log("hey i am abhay");
+    console.log("hey i am manav");
+}
+
+function myFunctions (msg, n) {
+//parameter -> input
+console. log (msg * n) ;
+}
+
+function sum(x, y) {
+s = x + y;
+return s;
+
+}
+
+let val = sum(3, 4);
+console. log(val);
+
+myFunctions(7, 100); //argument
+
+myfunction();
+myfunction();
