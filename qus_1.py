10eea6a43b8ed253010f91c0e096103c4d6afc42
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2025-10-08 18:46:14 +0530

    Add files via upload

---
+++ qus_1.py
@@ -0,0 +1,20 @@
+str="& & & &  & & yhhjasdfk & &"
+print(str.count("&"))
+
+if(str.count("&")==8):
+    print("what'sup")
+else:
+    print("bhag lode")
+    
+    
+# 2
+movies=[]
+mov1=input("enter movie name")
+mov2=input("enter movie name")
+movies.append(input("enter movie name"))
+
+movies.append(mov1)
+movies.append(mov2)
+
+
+print(movies)
